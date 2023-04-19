0b4176f2a78b4265c0d78deefd7f639bdefcd03a
Yunior Vargas Hernández <yuniorvargas1966@gmail.com>
2023-04-19 01:15:43 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-console.log("Funciona el tablón.")
-
-function tablon() {
-    return (
-        document.getElementById("app").innerHTML = "<h5>El Tablón, Sosúa, República Dominicana.</h5><p>El Tablón es un lugar hermoso, ubicado en la desembocadura del rio Sosúa.</p>"
-    )
-};
-tablon()
