c71078817fe3f4ab0f755d68cf7373e4f31d831b
Yunior Vargas Hernández <yuniorvargas1966@gmail.com>
2023-04-27 00:58:06 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("El Tablón App.")
+
+function tablon() {
+    return (
+        document.getElementById("root").innerHTML =
+        "<header><div><p>El Tablón</p></header><main><p>El Tablón es un lugar hermoso, ubicado en la desembocadura del rio Sosúa, República Dominicana.</p></main></div>"
+    )
+
+};
+tablon()
